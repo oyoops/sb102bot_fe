12a8c0509062793833f18c956dffcfecfc031c98
oyoops <121578862+oyoops@users.noreply.github.com>
2023-09-28 01:01:21 -0400

    bad env var fix

--- api/load_county_table.js
+++ api/load_county_table.js
@@ -2,11 +2,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+    user: process.env("DB_USER"),
+    host: process.env("DB_HOST"),
+    database: process.env("DB_NAME"),
+    password: process.env("DB_PASS"),
+    port: process.env("DB_PORT"),
     ssl: {
         rejectUnauthorized: false
     }
