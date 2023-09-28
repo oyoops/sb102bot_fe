0cea106ff59749724c1051432b4ece78768010ab
oyoops <121578862+oyoops@users.noreply.github.com>
2023-09-28 14:44:49 -0400

    Update load_county_table.js

--- api/load_county_table.js
+++ api/load_county_table.js
@@ -29,29 +29,33 @@ module.exports = async (req, res) => {
 
     try {
         const countyQuery = `
-        SELECT county_name 
-        FROM florida_counties 
-        WHERE ST_Contains(geom, ST_SetSRID(ST_MakePoint($1, $2), 4326));
+            SELECT pm.county_name 
+                FROM public.parcels_master AS pm
+                WHERE ST_Contains(pm.geom, ST_SetSRID(ST_MakePoint($1, $2), 4326));
         `;
-
         const countyResult = await pool.query(countyQuery, [lng, lat]);
+        
         const countyName = countyResult.rows[0].county_name;
-
+        console.log("COUNTY:",countyName);
+        
         const dataQuery = `
-        SELECT 
-            fc.county_name, 
-            fc.area_median_income, 
-            fc.millage_rate,
-            ca.area_median_income AS county_amis_income,
-            cm.millage AS county_millage
-        FROM florida_counties AS fc
-        LEFT JOIN county_amis AS ca ON fc.county_name = ca.county_name
-        LEFT JOIN county_millages AS cm ON fc.county_name = cm.county_name
-        WHERE fc.county_name = $1;
+            SELECT 
+                fc.county_name, 
+                fc.area_median_income, 
+                fc.millage_rate,
+                ca.area_median_income AS county_amis_income,
+                cm.millage AS county_millage
+            FROM florida_counties AS fc
+            LEFT JOIN county_amis AS ca ON fc.county_name = ca.county_name
+            LEFT JOIN county_millages AS cm ON fc.county_name = cm.county_name
+            WHERE fc.county_name = $1;
         `;
-
         const dataResult = await pool.query(dataQuery, [countyName]);
 
+        // ...
+        // ...
+        // ...
+        
         res.status(200).json(dataResult.rows[0]);
     } catch (err) {
         console.error(err);
