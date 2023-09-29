8df9f3206bf5189ada4b3c0a68279e7a7f960a21
oyoops <121578862+oyoops@users.noreply.github.com>
2023-09-29 00:52:13 -0400

    fix

--- public/main.js
+++ public/main.js
@@ -1,8 +1,7 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('#searchForm');
     const countyTableBody = document.querySelector('#countyDataTable tbody');
-const rentsTableBody = document.querySelector('#countyMaxRentsTable tbody');
+    const rentsTableBody = document.querySelector('#countyMaxRentsTable tbody');  // Select the new table's tbody
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -15,36 +14,56 @@ const rentsTableBody = document.querySelector('#countyMaxRentsTable tbody');
         }
 
         try {
-            // First, geocode the address
             const geocodeEndpoint = `/api/geocode?address=${encodeURIComponent(address)}`;
             const geocodeResponse = await fetch(geocodeEndpoint);
+            if (!geocodeResponse.ok) {
+                throw new Error(`Server responded with ${geocodeResponse.status}: ${await geocodeResponse.text()}`);
+            }
             const geocodeData = await geocodeResponse.json();
+            if (!geocodeData.results || geocodeData.results.length === 0) {
+                throw new Error('No matching location found for the provided address.');
+            }
 
-            const lat = geocodeData.latitude;
-            const lng = geocodeData.longitude;
+            const lat = geocodeData.results[0].geometry.location.lat;
+            const lng = geocodeData.results[0].geometry.location.lng;
 
-            // Fetch county data using the new endpoint
             const countyDataEndpoint = `/api/load_county_table?lat=${lat}&lng=${lng}`;
             const countyDataResponse = await fetch(countyDataEndpoint);
             const countyData = await countyDataResponse.json();
 
-            // Populate the table with the fetched data
-            const row = `
+            if (!countyData.county_name) {
+                throw new Error('No data available for the selected location.');
+            }
+
+            // Populate the county data table
+            const countyRow = `
                 <tr>
                     <td>${countyData.county_name}</td>
-                    <td>${countyData.area_median_income}</td>
-                    <td>${countyData.millage_rate}</td>
                     <td>${countyData.county_amis_income}</td>
                     <td>${countyData.county_millage}</td>
                 </tr>
             `;
             countyTableBody.innerHTML = countyRow;
-document.querySelector('#countyDataTable').style.display = 'table';
-rentsTableBody.innerHTML = rentsRow;
-document.querySelector('#countyMaxRentsTable').style.display = 'table';
+
+            // Populate the max rents table
+            const rentsRow = `
+                <tr>
+                    <td>${countyData.max_rent_0bd_120ami}</td>
+                    <td>${countyData.max_rent_1bd_120ami}</td>
+                    <td>${countyData.max_rent_2bd_120ami}</td>
+                    <td>${countyData.max_rent_3bd_120ami}</td>
+                </tr>
+            `;
+            rentsTableBody.innerHTML = rentsRow;
+
         } catch (error) {
-            console.error('Error:', error);
-            alert('Failed to fetch data. Please try again.');
+            if (error.message.startsWith("Server responded with")) {
+                console.error('Server error:', error);
+                alert('There was an error with the server. Please try again later.');
+            } else {
+                console.error('Error:', error);
+                alert('Failed to fetch data. Please try again.');
+            }
         }
     });
 });
