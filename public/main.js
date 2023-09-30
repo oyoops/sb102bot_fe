7cbf9ef27a5fabfb594d6c04d01e052ed11d0cbf
oyoops <121578862+oyoops@users.noreply.github.com>
2023-09-29 23:06:06 -0400

    king bebebe

--- public/main.js
+++ public/main.js
@@ -15,41 +15,56 @@ document.addEventListener('DOMContentLoaded', function() {
 
         try {
             console.log('Part 1 -- Start: Geocoding + Database Lookup -> County Data');
+
             // Call the geocode API on input address
             const geocodeEndpoint = `/api/geocode?address=${encodeURIComponent(address)}`;
             document.querySelector('.loading').style.display = 'block';  // Show loading indicator
             const geocodeResponse = await fetch(geocodeEndpoint);
-            document.querySelector('.loading').style.display = 'none';  // Hide loading indicator
+            
+            // Check if fetch went through successfully
             if (!geocodeResponse.ok) {
+                console.log('ERROR! Geocode API call failed.');
                 throw new Error(`Server responded with ${geocodeResponse.status}: ${await geocodeResponse.text()}`);
             }
+
+            // Get the geocode data
             const geocodeData = await geocodeResponse.json();
+
+            // Check if geocode successfully returned any result(s)
             if (!geocodeData.results || geocodeData.results.length === 0) {
                 throw new Error('WHOOPS!!!!!!!!\nNo parcel found there.');
             }
 
-            // only proceed if we have lat/lng
+            // Only proceeds if geocode was successful
+            // (so even if we never get an address, we can still do lat/long stuff)
             const lat = geocodeData.results[0].geometry.location.lat;
             const lng = geocodeData.results[0].geometry.location.lng;
-
-            // Initialize the map
+            
+            // Set up the Google Map of the input address
             const mapOptions = {
                 center: { lat: lat, lng: lng },
                 zoom: 15,
                 mapTypeId: 'satellite'
             };
+
+            // Initialize the map
             const map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+             // Show the input address on the map
+            document.querySelector('mapInputAddress').style.display = 'block';
 
-
-            // only proceed if we have county data
+            // Check the PostgreSQL database for the county at this location (geocoded lat/long derived from address)
             const countyDataEndpoint = `/api/load_county_table?lat=${lat}&lng=${lng}`;
             const countyDataResponse = await fetch(countyDataEndpoint);
             const countyData = await countyDataResponse.json();
-            
+
+            // Check if PostgreSQL database successfully got the county at this location (geocoded lat/long derived from address)
             if (!countyData.county_name) {
                 throw new Error('No data available for the selected location.');
             }
+
+            // Only proceeds if we successfully got county data
+            // (which means we got a match at this lat/long in the database)
 
             // Populate the county data table
             const countyRow = `
@@ -83,8 +98,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // DONE with Part 1
             console.log('Part 1 -- Data successfully populated.');
 
-            // After successfully populating the tables,
-            // display the 'Development Program' inputs section (Part 2)
+            // Display the 'Development Program' inputs section (Part 2) after successfully populating the tables
             document.getElementById('developmentProgramInputSection').style.display = 'block';
 
         } catch (error) {
