2a3214fd6b58d890eb326be09956086d407f7ab7
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-03 15:21:46 -0400

    Update load_county_table.js

--- api/load_county_table.js
+++ api/load_county_table.js
@@ -37,9 +37,9 @@ module.exports = async (req, res) => {
             ORDER BY ST_Distance(geom, ST_SetSRID(ST_Point($1, $2), 4326))
             LIMIT 1;
         `;
-        const countySensitivity = 0.01;
+        
         console.log('County query starting...');
-        const countyResult = await pool.query(countyQuery, [lng, lat, countySensitivity]);
+        const countyResult = await pool.query(countyQuery, [lng, lat, 0.01]);
         console.log('County query complete.');
 
         if (countyResult.rows.length === 0) {
@@ -50,7 +50,7 @@ module.exports = async (req, res) => {
         const countyName = countyResult.rows[0].county_name;
         console.log("COUNTY:",countyName);
 
-        console.log('Starting data query...');
+        console.log('Starting COUNTY DATA query...');
         const dataQuery = `
             SELECT 
                 fc.county_name, 
@@ -71,6 +71,29 @@ module.exports = async (req, res) => {
         console.log('Data query complete.');
         console.log(`Data query returned ${dataResult.rowCount} rows.\nResponse: ${JSON.stringify(dataResult.rows[0])}`);
         
+        // ...
+
+        console.log('Starting PARCEL DATA query...');
+        const parcelDataQuery = `
+            WITH input_point AS (
+                SELECT ST_SetSRID(ST_MakePoint($1, $2), 4326) AS geom
+            )
+            
+            -- Find the closest parcel in Broward County and join with data_master
+            SELECT dm.*
+            FROM input_point AS ip, parcels_master AS pm
+            INNER JOIN data_master AS dm ON pm.parcelno = dm.PARCEL_ID
+            WHERE pm.county_name = dm.county_name AND pm.county_name = $3
+            AND ST_DWithin(ip.geom, pm.geom, 0.01) -- Preliminary distance filter in degrees
+            ORDER BY ST_Distance(ip.geom, pm.geom)
+            LIMIT 1;
+        `;
+        
+        const parcelDataResult = await pool.query(parcelDataQuery, [lng, lat, countyName]);
+        console.log('Parcel data query complete.');
+        console.log(`Parcel data query returned ${parcelDataResult.rowCount} rows.\nResponse: ${JSON.stringify(parcelDataResult.rows[0])}`);
+        
+
         // ...
         
         console.log('Sending response to client.');
