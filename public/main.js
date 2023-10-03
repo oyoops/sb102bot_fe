55e061dac35e379ad8f5e232256409e933211200
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-03 16:15:12 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -257,6 +257,23 @@ document.addEventListener('DOMContentLoaded', function() {
                 "098": "Centrally assessed",
                 "099": "Acreage not zoned agricultural with or without extra features"
             };
+                        
+            const eligibleCodes = ['003', '010', '011', '012', '013', '014', '015', '016', '017', '018', '019', 
+                                    '020', '021', '022', '023', '024', '025', '026', '027', '028', '029', '030', 
+                                    '031', '032', '033', '034', '035', '036', '037', '038', '039', '040', '041', 
+                                    '042', '043', '044', '045', '046', '047', '048', '049'];
+
+            const eligibilityDiv = document.getElementById("eligibilityStatus");
+            
+            if (eligibleCodes.includes(parcelData.dor_uc)) {
+                eligibilityDiv.innerHTML = "This parcel is ELIGIBLE for Live Local Act development";
+                eligibilityDiv.style.color = "green";
+                eligibilityDiv.style.fontSize = "24px";
+            } else {
+                eligibilityDiv.innerHTML = "This parcel is NOT ELIGIBLE for Live Local Act development";
+                eligibilityDiv.style.color = "red";
+                eligibilityDiv.style.fontSize = "24px";
+            }
             
             // Populate the parcel data table
             const parcelDataRow = `
@@ -271,7 +288,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Display the parcel data table now that we have data
             document.getElementById('parcelDataTable').style.display = 'table'; // Display the parcel data table
-            
+
 
 
             // DONE with Part 1
