4178b34f3f665e33d1de7389a1bc1932b5fe3f3c
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-05 22:48:42 -0400

    fr

--- public/main.js
+++ public/main.js
@@ -455,13 +455,12 @@ document.addEventListener('DOMContentLoaded', function() {
                     aff3BD,
                     textModifier
             */
-            const textMod = ` Rave excessively about how great of a 'Live Local Act' opportunity this is. Utilize HTML extensively in your response for aesthetics; font, lists, line breaks, everything.`;
+            const textMod = ` Utilize HTML extensively in your response for aesthetics; font, lists, line breaks, everything.`;
 
             aiContainer.style.display = 'block';
-            aiContainer.innerHTML = `<i><h6>Drafting an investment memo, please wait...<h6></i><br>${address}`;
+            aiContainer.innerHTML = `<i><h6>Drafting an investment memo, please wait...<h6></i>`;
             const icMemoEndpoint = `/api/ask_ai?address=${encodeURIComponent(address)}&county=${countyData.county_name}&acreage=${acreageInput.value}&totalUnits=${totalUnits}&affordablePct=${affordablePct}&affStudio=${countyData.max_rent_0bd_120ami}&aff1BD=${countyData.max_rent_1bd_120ami}&aff2BD=${countyData.max_rent_2bd_120ami}&aff3BD=${countyData.max_rent_3bd_120ami}&textModifier=${encodeURIComponent(textMod)}`;
             const icMemoResponse = await fetch(icMemoEndpoint);
-            aiContainer.style.display = 'block';
             icMemo = await icMemoResponse.text();
             console.log("IC Memo Received:", icMemo);
             aiContainer.innerHTML = icMemo;
