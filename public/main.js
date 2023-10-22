879773541f1bcc0c6e8d18504951de71a4e0df05
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-22 13:16:53 -0400

    Eligible? Not eligible? Who knows!

--- public/main.js
+++ public/main.js
@@ -198,14 +198,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Get detailed eligibility
             if (maybeEligibleCodes.includes(parcelData.dor_uc)) {
-                eligibilityDiv.innerHTML += `<h3>The property is probably <u>NOT ELIGIBLE</u> for Live Local development.</h3> <br>It can't <i>already</i> be a residential/multifamily property to qualify.`;
+                eligibilityDiv.innerHTML += `<h3>The property is probably <u>NOT</u> ELIGIBLE for Live Local development...</h3> <br>It can't <i>already</i> be a residential/multifamily property to qualify.`;
                 eligibilityDiv.style.color = "Orange";
                 eligibilityDiv.style.fontSize = "18px";
             } else if (eligibleCodes.includes(parcelData.dor_uc)) {
                 buildingHeight = parseFloat(buildingHeight);
                 console.log("HEIGHT:", buildingHeight, "feet");
-                eligibilityDiv.innerHTML += `<h3>The property looks <u>ELIGIBLE</u> for Live Local Act development.</h3> 
-                    </br>That means -- among many other benefits -- you can <b>build as high as the tallest building</b> within a one-mile radius.</b>
+                eligibilityDiv.innerHTML += `<h3>The property looks <u>ELIGIBLE</u> for Live Local development!</h3> 
+                    </br>That means, among other benefits, you can <b>build as high as the tallest building</b> within a one-mile radius.</b>
                     </br>On this particular piece of dirt, your buildings could rise <b>up to ${buildingHeight.toFixed(0)} feet</b> in height!`
                 if (buildingHeight >= 200) {
                     eligibilityDiv.innerHTML += ` <i><b>Wow!</b> That's a lot of juicy feet 👀👣. </i>`;
@@ -213,7 +213,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 eligibilityDiv.style.color = "green";
                 eligibilityDiv.style.fontSize = "18px";
             } else {
-                eligibilityDiv.innerHTML += `<h3>The property looks <u>INELIGIBLE</u> for Live Local development.</h3> <br>It must be <b>commercial</b> or <b>industrial</b> <i>already</i> to qualify.`;
+                eligibilityDiv.innerHTML += `<h3>The property is probably <u>NOT</u> ELIGIBLE for Live Local development...</h3> <br>It must be <b>commercial</b> or <b>industrial</b> <i>already</i> to qualify.`;
                 eligibilityDiv.style.color = "red";
                 eligibilityDiv.style.fontSize = "18px";
             }
