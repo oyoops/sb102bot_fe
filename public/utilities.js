a3158f929187fcd954cbd39200491b5148107bc3
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-22 14:45:18 -0400

    fade in new method

--- public/utilities.js
+++ public/utilities.js
@@ -74,23 +74,25 @@ function animateTextFadeIn(element) {
   const original = element.cloneNode(true);
   element.innerHTML = '';  // Clear current content
 
-  // Flatten the nodes (text and HTML elements) into an array
-  const flattenNodes = (node) => {
-      if (node.childNodes.length === 0) {
-          return [node];
-      } else {
-          return Array.from(node.childNodes).flatMap(flattenNodes);
-      }
-  };
-  let nodes = flattenNodes(original);
-  let current = 0;
+  // Create a queue to handle nodes
+  let queue = Array.from(original.childNodes);
+  let current = queue.shift();
 
   let interval = setInterval(() => {
-      if (nodes[current].nodeName !== "#text" || nodes[current].textContent.trim() !== "") {
-          element.appendChild(nodes[current].cloneNode(true));  // Append the current node to the element
-      }
-      current++;
-      if (current === nodes.length) {
+      if (current) {
+          if (current.nodeName !== "#text" || current.textContent.trim() !== "") {
+              const appendedNode = element.appendChild(current.cloneNode(false));
+              if (current.childNodes.length > 0) {
+                  // If the current node has children, enqueue them to be processed next
+                  queue = [...Array.from(current.childNodes), ...queue];
+              }
+              if (appendedNode && current.childNodes.length > 0) {
+                  // If the appended node is a container, store it to append its children there
+                  element = appendedNode;
+              }
+          }
+          current = queue.shift();
+      } else {
           clearInterval(interval);
       }
   }, 200);  // Adjust this interval time to change the animation speed
