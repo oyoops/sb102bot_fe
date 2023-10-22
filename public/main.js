af87844a4a34bef8673af2f922a7580495a3a180
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-22 01:32:38 -0400

    asshull wet

--- public/main.js
+++ public/main.js
@@ -263,11 +263,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 eligibilityDiv.style.fontSize = "18px";
             }
 
-
-            // display the AI summary (appending it to eligibilityDiv as a shortcut)
-            const aiSummaryHtml = displayAiEnhancements(enhancements);
+            // display the AI summary (shortcut: append it to eligibilityDiv. VERY UGLY)
+            const aiSummaryHtml = displayAiEnhancements(aiEnhancements);
             eligibilityDiv.innerHTML = eligibilityDiv.innerHTML + "\n\n<i><u>Some thoughts on the property:</u></i>\n" + aiSummaryHtml;
-
 
             // convert land sq. ft. to acres
             acres = parseFloat(parcelData.lnd_sqfoot) / 43560;
