f8cd0845b7e537241669019965aa011890ed28ab
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-24 23:45:57 -0400

    a

--- public/utilities.js
+++ public/utilities.js
@@ -9,56 +9,7 @@
 
 /* AI-Related Functions: */
 
-async function generateRefinedSummary(sheetPublicCSVUrl, columnLetter, parcelData) {
-  ////const combinedResponses = await fetchColumnFromPublicSheet(sheetPublicCSVUrl, columnLetter);
-  ////console.log("X: " + combinedResponses);
 
-  // Construct a prompt for the AI to refine the combined responses
-  const prompt = `
-      CONTEXT:
-          Florida's Live Local Act, effective July 1, 2023, revolutionizes multifamily development by overriding municipal restrictions. Key provisions mandate that cities/counties approve multifamily developments if:
-              1. Over 40% of units are 'affordable' (affordable rent maximums vary by county).
-              2. There at least 70 affordable units.
-              3. All non-density/height/zoning/land use municipal regulations are met.
-          The Act's transformative benefits include bypassing lengthy public hearings, achieving the highest unit density anywhere within the municipality, and allowing structures to rise as tall as the tallest building within a mile.
-          Furthermore, it offers a 75% property tax abatement on affordable units set at 120% AMI level, equating to a net 30% property tax reduction for the entire development. 
-          
-      ROLE:
-          You are 'Live Local Buddy', the AI brains powering a web app that helps developers navigate this game-changing law. You help users find profitable properties to buy for building new apartment complexes.
-
-      BACKGROUND:
-          I used a set of related prompts with data to generate insights about a land parcel. Each was narrow, fseveral different aspects of it.
-          The five AI responses were then combined in order; as a result, it's now long, boring, and repetitive (provided below).
-          The AI's writing and parcel data are imperfect, but YOU are the editor; the last line of defense between me and distributing unprofessional mediocre content to my critical audience.            
-          
-      RULES:
-          - Must be in HTML format. Use fun, artistic HTML text styles and colors to emphasize key info, and use </br> for line breaks.
-          - Include plenty of emojis for emphasis throughout.  
-          - Remove all references to unavailable or incomplete information.
-          - Remove everything that isn't substantive or valuable.
-
-      SPEECH/PERSONA:
-          - You speak in the manner of a stereotypical cartoon robot.
-
-      YOUR TASK:
-          - Completely rewrite the crudely-combined AI responses (below). You will now write a well-formatted, concise evaluation about the viability of a user's parcel for development.
-          - Focus primarily on the Live Local Act pathway to build apartments if the parcel is currently zoned commercial or industrial. If not, then focus on apartments via obtaining traditional approvals.
-
-      ---
-  `;
-  //    ${combinedResponses}
-  //`;
-  
-  // Attach this prompt to parcelData and call fetchAiResponsesCombined
-  parcelData.prompt = prompt;
-  const refinedSummaryResponses = await fetchAiResponsesCombined(parcelData); // <<<<<<----------------------- WRONG               THIS SHOULD BE A SUMMARIZER/REFINER/EDITOR PROMPT FUNCTION
-  
-  // (this needs to go)
-  //// assuming contents are all in plain text format, join components together for a combined summary
-  const refinedSummary = refinedSummaryResponses.join(' ');
-
-  return refinedSummary;
-}
 
 // fetch the set of AI responses
 async function fetchAiResponsesCombined(row) {
@@ -152,8 +103,8 @@ async function fetchAiResponsesCombined(row) {
     ...row
   };
 
-  // map prompts to their endpoints then fetch all simultaneously
-  const queryString = new URLSearchParams(row).toString(); // convert to string
+  // map prompts to endpoints, then fetch all simultaneously
+  const queryString = new URLSearchParams(row).toString();
   const fetchPromises = endpoints.map(endpoint => {
       return fetch(`${endpoint}?${queryString}`)
       .then(response => {
@@ -169,37 +120,38 @@ async function fetchAiResponsesCombined(row) {
       });
   });
 
+  // Wait until all prompts receive their responses to return the combined version
   try {
-      // Wait for all prompts to receive responses
       const results = await Promise.all(fetchPromises);
       return results;
   } catch (error) {
-      // Error while fetching an AI response
-      const errorMessage = error?.data?.error?.message || "An unknown error occurred while fetching an AI response.";
-      console.error("Error while fetching an AI response:", errorMessage);
+      const errorMessage = error?.data?.error?.message || "Unknown error occurred while fetching AI response.";
+      console.error("Error while fetching AI response:", errorMessage);
       throw error;
   }
 }
 
-// combine the multiple AI responses
+// Combine all AI responses
 function composeAiResponsesCombined(aiResponses) {
-  console.log("  *** Received AI responses! ***  :-D");
+  console.log("  *** Received raw AI response collection! ***  :-D");
   if (!aiResponses || aiResponses.length === 0) {
       console.error("Error: No AI responses were received!");
       return;
   }
   
+  // preface final AI content with a custom introduction (may belong somewhere else...)
   let aiCombinationParts = [
       `</br></br>
       <h3 style="color:black;" align="center">
       First, let's review some preliminary intel.
-      </h3>
-      <ul>`
-  ];  // (the HTML intro really belongs somewhere else...)
+      </h3>`
+  ];
 
-  // Combine all AI responses by pushing each into one long string [HTML-formatted? maybe] 
+  // Combine all AI responses by pushing each into one long [HTML-formatted?] string 
+  aiCombinationParts.push("<ul>");
   aiResponses.forEach((aiResponse, index) => {
-    aiCombinationParts.push(`<li>${aiResponse}</li>`);
+    ////aiCombinationParts.push(`<li>${aiResponse}</li>`);
+    aiCombinationParts.push(`${aiResponse}`);
   });
   aiCombinationParts.push("</ul>");
 
@@ -208,13 +160,64 @@ function composeAiResponsesCombined(aiResponses) {
   return combinedResponses;
 }
 
+async function generateRefinedSummary(sheetPublicCSVUrl, columnLetter, parcelData) {
+  ////const combinedResponses = await fetchColumnFromPublicSheet(sheetPublicCSVUrl, columnLetter);
+  ////console.log("X: " + combinedResponses);
+
+  // Construct a prompt for the AI to refine the combined responses
+  const prompt = `
+      CONTEXT:
+          Florida's Live Local Act, effective July 1, 2023, revolutionizes multifamily development by overriding municipal restrictions. Key provisions mandate that cities/counties approve multifamily developments if:
+              1. Over 40% of units are 'affordable' (affordable rent maximums vary by county).
+              2. There at least 70 affordable units.
+              3. All non-density/height/zoning/land use municipal regulations are met.
+          The Act's transformative benefits include bypassing lengthy public hearings, achieving the highest unit density anywhere within the municipality, and allowing structures to rise as tall as the tallest building within a mile.
+          Furthermore, it offers a 75% property tax abatement on affordable units set at 120% AMI level, equating to a net 30% property tax reduction for the entire development. 
+          
+      ROLE:
+          You are 'Live Local Buddy', the AI brains powering a web app that helps developers navigate this game-changing law. You help users find profitable properties to buy for building new apartment complexes.
+
+      BACKGROUND:
+          I used a set of related prompts with data to generate insights about a land parcel. Each was narrow, fseveral different aspects of it.
+          The five AI responses were then combined in order; as a result, it's now long, boring, and repetitive (provided below).
+          The AI's writing and parcel data are imperfect, but YOU are the editor; the last line of defense between me and distributing unprofessional mediocre content to my critical audience.            
+          
+      RULES:
+          - Must be in HTML format. Use fun, artistic HTML text styles and colors to emphasize key info, and use </br> for line breaks.
+          - Include plenty of emojis for emphasis throughout.  
+          - Remove all references to unavailable or incomplete information.
+          - Remove everything that isn't substantive or valuable.
+
+      SPEECH/PERSONA:
+          - You speak in the manner of a stereotypical cartoon robot.
+
+      YOUR TASK:
+          - Completely rewrite the crudely-combined AI responses (below). You will now write a well-formatted, concise evaluation about the viability of a user's parcel for development.
+          - Focus primarily on the Live Local Act pathway to build apartments if the parcel is currently zoned commercial or industrial. If not, then focus on apartments via obtaining traditional approvals.
+
+      ---
+  `;
+  //    ${combinedResponses}
+  //`;
+  
+  // Attach this prompt to parcelData and call fetchAiResponsesCombined
+  parcelData.prompt = prompt;
+  const refinedSummaryResponses = await fetchAiResponsesCombined(parcelData); // <<<<<<----------------------- WRONG               THIS SHOULD BE A SUMMARIZER/REFINER/EDITOR PROMPT FUNCTION
+  
+  // (this needs to go)
+  //// assuming contents are all in plain text format, join components together for a combined summary
+  const refinedSummary = refinedSummaryResponses.join(' ');
+
+  return refinedSummary;
+}
+
 // fade in the AI response text
 function animateTextFadeIn(element) {
   if (!element) {
       console.error("Invalid animation.");
       return;
   }
-  
+
   const original = element.cloneNode(true);
   element.innerHTML = '';
 
