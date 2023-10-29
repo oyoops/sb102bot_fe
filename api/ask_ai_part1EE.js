061e89bf5d5c3957e06f6d3e064700569207958c
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-29 13:26:49 -0400

    "MEGA MUSHARNA" v1.416

--- api/ask_ai_part1EE.js
+++ api/ask_ai_part1EE.js
@@ -527,7 +527,7 @@ module.exports = async (req, res) => {
         const response = await axios.post('https://api.openai.com/v1/chat/completions', {
             model: 'gpt-3.5-turbo-16k',//process.env.AI_MODEL_PRIMARY_ANALYSES,
             messages: messages,
-            max_tokens: 500, //parseInt(process.env.AI_MAX_TOKENS_PRIMARY_ANALYSES, 10),
+            max_tokens: 750, //parseInt(process.env.AI_MAX_TOKENS_PRIMARY_ANALYSES, 10),
             temperature: 0.3,
             presence_penalty: 0.1,
             frequency_penalty: 0.1
