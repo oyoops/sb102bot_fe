025bebc8e79b0062d1676e4d6be837e731cc67cb
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-29 03:10:12 -0400

    "DONGDONG" v1.361

--- public/utilities.js
+++ public/utilities.js
@@ -445,7 +445,7 @@ function addLoadingLine(text) {
 // ...and so on.
 
 /* Update the fake loading progress bar */
-let percentageLoading = 0;
+//let percentageLoading = 0;
 const intervalTimeLoading = 100; // update every 100ms
 const totalUpdates = 600; // 60 seconds / 0.1 seconds
 let updateCount = 0; // number of times the function has been called
