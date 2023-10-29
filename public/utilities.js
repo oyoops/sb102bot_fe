1ec6429fc7f900c8d2987fb11d1c8de735cbe9e7
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-29 02:08:55 -0400

    20->30sec timeout

--- public/utilities.js
+++ public/utilities.js
@@ -39,7 +39,7 @@ async function fetchAiResponsesCombined(cleanData) {
             }
             return response.json();
         }),
-        timeout(20000) // 20 seconds
+        timeout(30000) // 30 seconds
     ])
     .catch(err => {
         console.error(`Error during fetch from endpoint ${endpoint}: ${err}`);
