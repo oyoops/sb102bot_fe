4f725eb28e9cd7f096e7829f12155f10e82feed7
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-30 15:51:57 -0400

    Update ask_ai_part1EE.js

--- api/ask_ai_part1EE.js
+++ api/ask_ai_part1EE.js
@@ -412,9 +412,10 @@ module.exports = async (req, res) => {
     try {
         // Send fetch request from server to OpenAI API
         const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-            model: 'gpt-3.5-turbo-16k',//process.env.AI_MODEL_PRIMARY_ANALYSES,
+            //model: 'gpt-3.5-turbo-16k',//process.env.AI_MODEL_PRIMARY_ANALYSES,
+            model: 'gpt-3.5-turbo',//process.env.AI_MODEL_PRIMARY_ANALYSES,
             messages: messages,
-            max_tokens: 1000, //parseInt(process.env.AI_MAX_TOKENS_PRIMARY_ANALYSES, 10),
+            max_tokens: 1200, //parseInt(process.env.AI_MAX_TOKENS_PRIMARY_ANALYSES, 10),
             temperature: 0.3,
             presence_penalty: 0.1,
             frequency_penalty: 0.1
