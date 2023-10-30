101e3205054c4e890aa4ad33f02afc3dea511ad8
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-30 16:24:56 -0400

    Update ask_ai_part1EE.js

--- api/ask_ai_part1EE.js
+++ api/ask_ai_part1EE.js
@@ -415,7 +415,7 @@ module.exports = async (req, res) => {
             //model: 'gpt-3.5-turbo-16k',//process.env.AI_MODEL_PRIMARY_ANALYSES,
             model: 'gpt-3.5-turbo',//process.env.AI_MODEL_PRIMARY_ANALYSES,
             messages: messages,
-            max_tokens: 1200, //parseInt(process.env.AI_MAX_TOKENS_PRIMARY_ANALYSES, 10),
+            max_tokens: 700, //parseInt(process.env.AI_MAX_TOKENS_PRIMARY_ANALYSES, 10),
             temperature: 0.3,
             presence_penalty: 0.1,
             frequency_penalty: 0.1
