6853ce3ebbe9f84166482fe621e1d563ecfc684e
oyoops <121578862+oyoops@users.noreply.github.com>
2023-10-31 18:05:51 -0400

    "DIRTDEVIL SUPREME" v1.716vi

--- api/ask_ai_part1SER.js
+++ api/ask_ai_part1SER.js
@@ -59,9 +59,9 @@ module.exports = async (req, res) => {
                 ROLE:
                     You are 'Live Local Buddy', the AI brains powering a web app that helps developers navigate this game-changing law.
                     You help users find and analyze profitable properties to buy for multifamily development.
-                    You specialize in the new Florida law called the Live Local Act.
+                    You specialize in Florida's new 'Live Local Act' law.
                 BACKGROUND:
-                    A set of related prompts with data were used to generate insights about a property. Each prompt was narrow, examining a different aspect of the land.
+                    A set of related prompts with data were used to generate insights about a property, then combined.
                 SPEECH/PERSONA:
                     You are a real estate development robot giving an analysis of a site for potential multifamily development.
                 FORMAT RULES:
@@ -70,28 +70,20 @@ module.exports = async (req, res) => {
                     - HTML tables are welcome for tabular data.
                     - Use <b>tags for all subheadings.
                     - Only use line breaks SPARINGLY. NEVER USE CONSECUTIVE LINE BREAKS!!!
-            `
-    }, {
-        "role": "user",
-        "content": `
-                YOUR TASKS:
-                    Respond in two distinct sections with a horizontal separator.
-                    (1) Write a detailed internal report about the property. Be advanced, but include emojis.
-                        The report shall consider the detailed explanation of the Act provided.
-                    (2) Write a formal nonbinding LOI to the owner for purchasing the property.
-                PARAMETERS:
-                    Offer price shall be $20,000 per 'affordable' unit + $40,000 per market-rate unit.
-                    We shall always do the minimum affordable units required by eligibility thresholds.
-                    Assume our unit count for purposes of the offer = MIN(${suppDataForAI.maxCapacity}, 500).
                 COMMON PITFALL:
                     - The combined AI responses COULD be wrong about eligibility.
                         * If there is no possible way to get to 70 total affordable units (if ${suppDataForAI.maxCapacity} < 70) while the parcel may be eligible, it is actually NOT POSSIBLE.
                             Point this 'technical ineligibility' out in MASSIVE RED ALERT FONT.
-                            If this is the case, you MUST override the content, say it is INELIGIBLE in the Report and LOI, and then decline to make an offer to purchase the property alone.
-                                Instead, if the landowner can find an adjacent neighbor with enough acreage to achieve the necessary unit count by agreeing to combine parcels, we'd offer both of them their pro rata share at the original offer prices per unit of $20,000 per 'affordable' unit and $40,000 per market-rate unit (so 100% @ $20,000 * 70 units in the absolute minimum case).
-                                Calculate the minimum number of acres we'd need if we were to do 100% affordable (in other words, 70 affordable units) and include it in the LOI.
-                OTHER RULES:
-                    - In the LOI, don't explicitly mention the maximum units/acre density allowed in the municipality.                
+                            If this is the case, you MUST override the content, say it is INELIGIBLE in the Report.
+                            Calculate the minimum number of acres we'd need if we were to do 100% affordable (in other words, 70 affordable units) and include it.
+            `
+    }, {
+        "role": "user",
+        "content": `
+                YOUR TASK:
+                    Write a detailed internal report about the property.
+                    Be advanced, but include emojis.
+                    The report shall consider the detailed explanation of the Act provided.
                 ---
                 ${aiCombinedResponses}
             `
@@ -104,7 +96,7 @@ module.exports = async (req, res) => {
         if (superAI == 'on') {
             console.log('[SuperAI is ON]');
             useModel = 'gpt-4';
-            useTokens = 500;
+            useTokens = 400;
         } else {
             console.log('[SuperAI is OFF]');
             useModel = process.env.AI_MODEL_SER_MODULE;
