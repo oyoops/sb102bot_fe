1b9d17265a78a1596cabc87fbff9f0ce6aa93075
oyoops <121578862+oyoops@users.noreply.github.com>
2023-11-20 16:17:59 -0500

    SECOND STAGE RELEASE! PERFECT2

--- public/domElements.js
+++ public/domElements.js
@@ -1,106 +1,45 @@
-// domElements.js
+// domElements.js - Define and export DOM elements for main.js (ES6 Module Syntax)
 
-/* (Option 1) ES6 Module Syntax */
 
 // Div sections/containers
 const mainHeader = document.getElementById("mainHeader");
 const loadingContainer = document.querySelector('.loading-container');
 const initialContent = document.querySelector('#initialContent');
-//const navButtonsContainer = document.querySelector('#navButtonsContainer');
-
 const infoSections = document.getElementById('infoSections');
-const currentBugsContainer = document.getElementById('currentBugs');
 const recentUpdatesContainer = document.getElementById('recentUpdates');
-const futureUpdatesContainer = document.getElementById('futureUpdates');
 
-const eligibilityDiv = document.getElementById("eligibilityStatus");
-const compsTable = document.getElementById("compsTable");
-
-const developmentProgramInputSection = document.getElementById('developmentProgramInputSection');
-const rentInfoContainer = document.getElementById('rentInfoContainer');
-/*const marketRateInputSection = document.getElementById('marketRateInputSection');
-const rentPerSqFtTableSection = document.getElementById('rentPerSqFtTableSection');
-const landAndTotalHcInputSection = document.getElementById('landAndTotalHcInputSection');
-const landAndTotalHcOutputSection = document.getElementById('totalLandAndTotalHcOutputSection');
-
-// Tables & bodies
-//const parcelDataTable = document.getElementById('parcelDataTable');
-//const parcelDataTableBody = document.querySelector('#parcelDataTable tbody');
-const countyDataTable = document.getElementById('countyDataTable');
-const countyTableBody = document.querySelector('#countyDataTable tbody');
-*/
-const countyMaxRentsTable = document.getElementById('countyMaxRentsTable');
-const rentsTableBody = document.querySelector('#countyMaxRentsTable tbody');
-/*
-const unitCalculationTable = document.getElementById('unitCalculationTable');
-const abatementTable = document.getElementById('abatementTable');
-*/
-
-// Form and its elements
+// Main form and its elements
 const form = document.querySelector('#searchForm');
 const addressInput = document.querySelector('#addressInput');
-
-/*
-const affordablePercentageSlider = document.getElementById("affordablePctSlider");
-const affordablePctDisplay = document.getElementById('affordablePctDisplay');
-const acreageInput = document.getElementById("acreageInput");
-const densityInput = document.getElementById('densityInput');
-const landCostPerUnit = document.getElementById('landCostPerUnitInput');
-const totalHCPerUnit = document.getElementById('totalHCPerUnitInput');
-const matchAffordableSizesCheckbox = document.getElementById('matchAffordableSizes');
-
-// Input groupings
-const sizeInputs = document.querySelectorAll('.sizeInput');
-const marketInputs = document.querySelectorAll('.marketSizeInput');
-const affordableSizeInputs = document.querySelectorAll('.affordableSizeInput');
-const marketRateInputs = document.querySelectorAll('.marketRateInput');
-*/
+/* Form is missing the two switches! */
 
 // Map and try again button
 const googlemap = document.getElementById('map');
 const tryAgainButton = document.getElementById("tryAgainButton");
 
-export {
-    loadingContainer,
-    initialContent,
-    //navButtonsContainer,
+// AI response div
+const eligibilityDiv = document.getElementById("eligibilityStatus");
 
-    infoSections,
-    currentBugsContainer,
-    recentUpdatesContainer,
-    futureUpdatesContainer,
-    
-    eligibilityDiv,
-    compsTable,
-    
-    rentInfoContainer,
-    //developmentProgramInputSection,
-    //marketRateInputSection,
-    //rentPerSqFtTableSection,
-    //landAndTotalHcInputSection,
-    //landAndTotalHcOutputSection,
-    mainHeader,
-    //parcelDataTable,
-    //parcelDataTableBody,
-    //countyDataTable,
-    //countyTableBody,
-    countyMaxRentsTable,
-    rentsTableBody,
-    //unitCalculationTable,
-    //abatementTable,
+// Tables and their elements
+const rentInfoContainer = document.getElementById('rentInfoContainer');
+const countyMaxRentsTable = document.getElementById('countyMaxRentsTable');
+const rentsTableBody = document.querySelector('#countyMaxRentsTable tbody');
+const compsTable = document.getElementById("compsTable");
+
+
+export {
+    initialContent,
+    mainHeader, // (?)
     form,
     addressInput,
-    //affordablePercentageSlider,
-    //affordablePctDisplay,
-    //acreageInput,
-    //densityInput,
-    //landCostPerUnit,
-    //totalHCPerUnit,
-    //matchAffordableSizesCheckbox,
-    //sizeInputs,
-    //marketInputs,
-    //affordableSizeInputs,
-    //marketRateInputs,
+    infoSections,
+    recentUpdatesContainer,
+    loadingContainer,
     googlemap,
-    tryAgainButton
+    tryAgainButton,
+    eligibilityDiv,
+    compsTable,
+    rentInfoContainer,
+    countyMaxRentsTable,
+    rentsTableBody
 };
