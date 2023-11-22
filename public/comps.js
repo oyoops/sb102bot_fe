110007cfdb7f67a053b26bc60a4da24a304a92ae
oyoops <121578862+oyoops@users.noreply.github.com>
2023-11-22 12:26:02 -0500

    s

--- public/comps.js
+++ public/comps.js
@@ -78,8 +78,84 @@ function displayCompsTable(compsData) {
 
 // Generate Table #1 HTML from comps data
 function generateMarketRentsTableHTML(compsData) {
-    // Original implementation of generateMarketRentsTableHTML
-    // ...
+    const averages = compsData.averages;
+    const percentages = compsData.percentages;
+    const names = compsData.names;
+
+    const types = [
+        { key: 'studio', display: 'Studio' },
+        { key: 'oneBd', display: '1 BD' },
+        { key: 'twoBd', display: '2 BD' },
+        { key: 'threeBd', display: '3 BD' },
+    ];
+
+    let tableHTML = `
+        <div class="table-container">
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>% Mix</th>
+                        <th>Avg. SF</th>
+                        <th>Market Rent</th>
+                        <th>Market Rent/SF</th>
+                    </tr>
+                </thead>
+                <tbody>
+    `;
+
+    let weightedRentSum = 0;
+    let weightedSqftSum = 0;
+    let weightedRentPerSqftSum = 0;
+    let percentageSum = 0;
+
+    types.forEach(type => {
+        const rent = averages.rents[type.key];
+        const sqft = averages.sqfts[type.key];
+        const rentPerSqft = averages.rentPerSqfts[type.key];
+        const weight = parseFloat(percentages[type.key] || '0');
+
+        // Accumulate the weighted sums
+        weightedRentSum += rent * weight;
+        weightedSqftSum += sqft * weight;
+        weightedRentPerSqftSum += rentPerSqft * weight;
+        percentageSum += weight;
+
+        tableHTML += `
+                <tr>
+                    <td>${type.display}</td>
+                    <td>${weight.toFixed(0)}%</td>
+                    <td>${sqft.toFixed(0)} SF</td>
+                    <td>$${rent.toFixed(0)}</td>
+                    <td>$${rentPerSqft.toFixed(2)}/SF</td>
+                </tr>
+        `;
+
+    });
+    // Calculate the weighted averages
+    const averageWeightedRent = (weightedRentSum / 100).toFixed(0);
+    const averageWeightedSqft = (weightedSqftSum / 100).toFixed(0);
+    const averageWeightedRentPerSqft = (weightedRentPerSqftSum / 100).toFixed(2);
+    
+    // Cap percentage sum manually since the rounding handling sucks
+    percentageSum = Math.min(percentageSum, 100);
+
+    // Append the weighted average row
+    tableHTML += `
+            <tr>
+                <td><strong>Avgs.</strong></td>
+                <td><strong>${percentageSum.toFixed(0)}%</strong></td>
+                <td><strong>${averageWeightedSqft} SF</strong></td>
+                <td><strong>$${averageWeightedRent}</strong></td>
+                <td><strong>$${averageWeightedRentPerSqft}/SF</strong></td>
+            </tr>
+    `;
+    tableHTML += `
+                </tbody>
+            </table>
+        </div>
+    `;
+    return tableHTML;
 }
 
 // Generate table HTML for Table #2 (Comp avg. rents vs. affordable max. rents by unit type)
